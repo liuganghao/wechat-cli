c8e7626d0fd438542c3c3fa7e595e377e1fdd0de
liugh <liuganghao@lztogether.com>
2017-12-20 15:09:42 +0800

    commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // 导入基本模块
 var wx = require('./lib/session')
-var storage = require('./lib/localdb').message
-var RemoteDB = require('./lib/leandb')
+// var storage = require('./lib/localdb').message
+// var RemoteDB = require('./lib/leandb')
 
 var _ = require('lodash')
 var _debug = require('debug')
-var retry = require('async-retry')
 const setting = require('./rt/demo_turing.json');
 const debug = _debug('nodecli')
 const qrcode = require('qrcode-terminal')
